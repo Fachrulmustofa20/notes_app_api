f42bb7f9b39e678220ef372dc303ea7f2e6332ea
Fachrulmustofa20 <fachrulmustofa100@gmail.com>
2021-09-24 15:33:42 +0700

    add validation

--- src/api/notes/index.js
+++ src/api/notes/index.js
@@ -1,6 +1,6 @@
 const NotesHandler = require('./handler');
 const routes = require('./routes');
- 
+
 module.exports = {
   name: 'notes',
   version: '1.0.0',
--- src/services/inMemory/NotesService.js
+++ src/services/inMemory/NotesService.js
@@ -1,34 +1,34 @@
 const { nanoid } = require('nanoid');
- 
+
 class NotesService {
   constructor() {
     this._notes = [];
   }
- 
+
   addNote({ title, body, tags }) {
     const id = nanoid(16);
     const createdAt = new Date().toISOString();
     const updatedAt = createdAt;
- 
+
     const newNote = {
       title, tags, body, id, createdAt, updatedAt,
     };
- 
+
     this._notes.push(newNote);
- 
+
     const isSuccess = this._notes.filter((note) => note.id === id).length > 0;
- 
+
     if (!isSuccess) {
       throw new Error('Catatan gagal ditambahkan');
     }
- 
+
     return id;
   }
- 
+
   getNotes() {
     return this._notes;
   }
- 
+
   getNoteById(id) {
     const note = this._notes.filter((n) => n.id === id)[0];
     if (!note) {
@@ -36,16 +36,16 @@ class NotesService {
     }
     return note;
   }
- 
+
   editNoteById(id, { title, body, tags }) {
     const index = this._notes.findIndex((note) => note.id === id);
- 
+
     if (index === -1) {
       throw new Error('Gagal memperbarui catatan. Id tidak ditemukan');
     }
- 
+
     const updatedAt = new Date().toISOString();
- 
+
     this._notes[index] = {
       ...this._notes[index],
       title,
@@ -54,7 +54,7 @@ class NotesService {
       updatedAt,
     };
   }
- 
+
   deleteNoteById(id) {
     const index = this._notes.findIndex((note) => note.id === id);
     if (index === -1) {
